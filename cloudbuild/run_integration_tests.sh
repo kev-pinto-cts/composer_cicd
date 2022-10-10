a443b027a63736e8bb0ab71e84713137d167a5ca
kev-pinto-cts <kev.pinto@cts.co>
2022-10-10 19:23:45 +0100

    Testing Cloud Build

--- cloudbuild/run_integration_tests.sh
+++ cloudbuild/run_integration_tests.sh
@@ -5,18 +5,19 @@ SHORT_SHA=$2
 LOCATION=$3
 
 echo "BRANCH=${BRANCH} COMMIT_SHA=${SHORT_SHA}"
+pwd
 
-project_to_branch_map=($(cat ../config/env_mapper.txt))
-for mapping in ${project_to_branch_map[@]}; do
-  project_id=$(echo ${mapping}|cut -d":" -f1)
-  dag_bucket=$(echo ${mapping}|cut -d":" -f2)
-  branch=$(echo ${mapping}|cut -d":" -f3)
-
-
-  if [ ${BRANCH} == ${branch} ]; then
-    gcloud config set project ${project_id}
-    gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
-    gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
-    gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
-  fi
-done
+#project_to_branch_map=($(cat ../config/env_mapper.txt))
+#for mapping in ${project_to_branch_map[@]}; do
+#  project_id=$(echo ${mapping}|cut -d":" -f1)
+#  dag_bucket=$(echo ${mapping}|cut -d":" -f2)
+#  branch=$(echo ${mapping}|cut -d":" -f3)
+#
+#
+#  if [ ${BRANCH} == ${branch} ]; then
+#    gcloud config set project ${project_id}
+#    gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
+#    gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
+#    gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
+#  fi
+#done
