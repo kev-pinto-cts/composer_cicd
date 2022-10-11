07740fa6fe8e30f7d2d1c7dea7cfaa0cec6223b6
kev-pinto-cts <kev.pinto@cts.co>
2022-10-11 11:01:07 +0100

    ON the TPE

--- cloudbuild/run_integration_tests.sh
+++ cloudbuild/run_integration_tests.sh
@@ -8,15 +8,14 @@ echo "BRANCH=${BRANCH} COMMIT_SHA=${SHORT_SHA}"
 
 project_to_branch_map=($(cat ./config/env_mapper.txt))
 for mapping in ${project_to_branch_map[@]}; do
-  project_id=$(echo ${mapping}|cut -d":" -f1)
-  dag_bucket=$(echo ${mapping}|cut -d":" -f2)
-  branch=$(echo ${mapping}|cut -d":" -f3)
-
+  project_id=$(echo ${mapping} | cut -d":" -f1)
+  dag_bucket=$(echo ${mapping} | cut -d":" -f2)
+  branch=$(echo ${mapping} | cut -d":" -f3)
 
   if [ ${BRANCH} == ${branch} ]; then
     gcloud config set project ${project_id}
     gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
     gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
-    gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
+    #    gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
   fi
 done
