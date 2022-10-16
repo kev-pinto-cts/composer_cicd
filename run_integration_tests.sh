220aaf1a80c9eb95f301c1c0a980916f6841f288
kev-pinto-cts <kev.pinto@cts.co>
2022-10-16 15:24:36 +0100

    Sunday Morning

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -18,6 +18,7 @@ for mapping in ${project_to_branch_map[@]}; do
   if [ ${BRANCH} == ${branch} ]; then
     gcloud config set project ${project_id}
     gsutil rsync -r -d dags/ ${dag_bucket}/data/${SHORT_SHA}/
+    gcloud config list
     echo "gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/"
     gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
 #    gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
