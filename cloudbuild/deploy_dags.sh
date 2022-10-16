1decccf3b59678febdfbfc93c6856178536d15cc
kev-pinto-cts <kev.pinto@cts.co>
2022-10-16 16:35:46 +0100

    Stable Commit

---
+++ cloudbuild/deploy_dags.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+BRANCH=$1
+SHORT_SHA=$2
+LOCATION=$3
+
+echo "BRANCH=${BRANCH} COMMIT_SHA=${SHORT_SHA} -- LOCATION--${LOCATION}"
+
+project_to_branch_map=($(cat /workspace/config/env_mapper.txt))
+for mapping in ${project_to_branch_map[@]}; do
+  project_id=$(echo ${mapping} | cut -d"~" -f1)
+  dag_bucket=$(echo ${mapping} | cut -d"~" -f2 | sed "s/\/dags//g")
+  branch=$(echo ${mapping} | cut -d"~" -f3)
+  echo "${project_id}--${dag_bucket}--${branch}"
+
+  if [ ${BRANCH} == ${branch} ]; then
+    target=${dag_bucket}/dags
+    gcloud config set project ${project_id}
+    gsutil rsync -r -d dags/ ${target}/
+  fi
+done
