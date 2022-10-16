bf6cbbf9476d3f6fb5e52d966103c5e13e9b3a79
kev-pinto-cts <kev.pinto@cts.co>
2022-10-16 13:47:35 +0100

    Sunday Morning

---
+++ run_integration_tests.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+BRANCH=$1
+SHORT_SHA=$2
+LOCATION=$3
+
+pwd
+echo "BRANCH=${BRANCH} COMMIT_SHA=${SHORT_SHA} -- LOCATION--${LOCATION}"
+
+
+project_to_branch_map=($(cat ./config/env_mapper.txt))
+for mapping in ${project_to_branch_map[@]}; do
+  project_id=$(echo ${mapping} | cut -d":" -f1)
+  dag_bucket=$(echo ${mapping} | cut -d":" -f2)
+  branch=$(echo ${mapping} | cut -d":" -f3)
+
+  if [ ${BRANCH} == ${branch} ]; then
+    gcloud config set project ${project_id}
+    gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
+    gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
+#    gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
+  fi
+done
